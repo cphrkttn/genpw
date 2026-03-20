8b666e680aea75019725ed264732d55bc1ad447f
cphrkttn <rachelsandover@outlook.com>
2026-03-19 17:56:04 -0700

    error handling, proper malloc, handle pw as string

--- genpw.c
+++ genpw.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 
 
-void pwgen(int N) {
+char* pwgen(int N) {
     int i = 0;
     int randomizer = 0;
-
-
-    // seed prng with current time
-    srand(time(NULL));
 
     // Arrays
     char number[] = "0123456789";
@@ -18,7 +13,11 @@ void pwgen(int N) {
     char letterUpper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char special[] = "!@#$^&*?";
 
-    char password[N];
+    char* password = malloc(N + 1);
+    if (password == NULL) {
+	    fprintf(stderr, "Error: memory allocation failed.\n");
+	    exit(EXIT_FAILURE);
+    }
 
     randomizer = rand() % 4;
 
@@ -27,40 +26,57 @@ void pwgen(int N) {
 	    case 1:
 		    password[i] = number[rand() % 10];
 		    randomizer = rand() % 4;
-		    printf("%c", password[i]);
+		    //printf("%c", password[i]);
 		    break;
 	    case 2:
 		    password[i] = special[rand() % 8];
                     randomizer = rand() % 4;
-                    printf("%c", password[i]);
+                    //printf("%c", password[i]);
 		    break;
 	    case 3:
 		    password[i] = letterUpper[rand() % 26];
                     randomizer = rand() % 4;
-                    printf("%c", password[i]);
+                    //printf("%c", password[i]);
 		    break;
 	    default:
 		    password[i] = letterLower[rand() % 26];
                     randomizer = rand() % 4;
-                    printf("%c", password[i]);
+                    //printf("%c", password[i]);
 		    break;
 	}
     }
-    printf("\n");
+    //printf("\n");
+    password[N] = '\0';
+    return password;
 }
-
-
 
 
 int main(int argc, char** argv) {
 
-    if (argc < 2 || argc > 2) {
-	fprintf(stderr, "Syntax error!\n");
+    // if arg count less than two throw error
+    if (argc != 2) {
 	fprintf(stderr, "Usage: %s <length>\n", argv[0]);
 	exit(EXIT_FAILURE);
-   } else {
-        int N = atoi(argv[1]);
-        pwgen(N);
-   }
+    }
+    
+    // argc is good so init N
+    int N = atoi(argv[1]);
+
+    // check N because atoi doesn't handle errors
+    if (N <= 0) {
+	fprintf(stderr, "Error: length must be a positive integer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    // seed prng with current time
+    srand(time(NULL));
+
+    // if all ok run function
+    char* pw = pwgen(N);
+    printf("%s\n", pw);
+    free(pw);
+
+
     return 0;
 }
