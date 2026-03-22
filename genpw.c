efb5dbde69fcd2f72faa25ad2b21c429c26f8f33
cphrkttn <rachelsandover@outlook.com>
2026-03-22 10:29:14 -0700

    use /dev/urandom, minor tweaks

--- genpw.c
+++ genpw.c
@@ -11,7 +11,7 @@ char* pwgen(int N) {
     char number[] = "0123456789";
     char letterLower[] = "abcdefghijklmnopqrstuvwxyz";
     char letterUpper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char special[] = "!@#$^&*?";
+    char special[] = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
 
     char* password = malloc(N + 1);
     if (password == NULL) {
@@ -26,26 +26,22 @@ char* pwgen(int N) {
 	    case 1:
 		    password[i] = number[rand() % 10];
 		    randomizer = rand() % 4;
-		    //printf("%c", password[i]);
 		    break;
 	    case 2:
-		    password[i] = special[rand() % 8];
+		    password[i] = special[rand() % 29];
                     randomizer = rand() % 4;
-                    //printf("%c", password[i]);
 		    break;
 	    case 3:
 		    password[i] = letterUpper[rand() % 26];
                     randomizer = rand() % 4;
-                    //printf("%c", password[i]);
 		    break;
 	    default:
 		    password[i] = letterLower[rand() % 26];
                     randomizer = rand() % 4;
-                    //printf("%c", password[i]);
 		    break;
 	}
     }
-    //printf("\n");
+    
     password[N] = '\0';
     return password;
 }
@@ -68,9 +64,17 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    
-    // seed prng with current time
-    srand(time(NULL));
+    // seed rand() with urandom
+    unsigned int seed;
+    FILE* f = fopen("/dev/urandom", "rb");
+    if (f == NULL) {
+        fprintf(stderr, "Error: could not open /dev/urandom\n");
+	exit(EXIT_FAILURE);
+    }
+    fread(&seed, sizeof(unsigned int), 1, f);
+    fclose(f);
+    srand(seed);
+
 
     // if all ok run function
     char* pw = pwgen(N);
